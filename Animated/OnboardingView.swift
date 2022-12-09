06d461301218f6f1e634cb6494a3bb70ef5c9d74
fbladimir <franklinbladimirck@yahoo.com>
2022-12-09 17:26:16 -0500

    added button to onboarding view with working animation

--- Animated/OnboardingView.swift
+++ Animated/OnboardingView.swift
@@ -9,15 +9,50 @@ import SwiftUI
 import RiveRuntime
 
 struct OnboardingView: View {
+    
+    //declare the riveasset in the beginning
+    let button = RiveViewModel(fileName: "button")
+    
     var body: some View {
-        RiveViewModel(fileName: "franky").view()
-            .ignoresSafeArea()
-            .blur(radius: 30)
-            .background(
-                Image("Spline")
-                    .blur(radius: 50)
-                    .offset(x:200, y:100)
+        ZStack {
+            //adding background view
+            RiveViewModel(fileName: "franky").view()
+                .ignoresSafeArea()
+                .blur(radius: 30)
+                .background(
+                    Image("Spline")
+                        .blur(radius: 50)
+                        .offset(x:200, y:100)
             )
+            
+            //Adding the declared button to view it 
+            button.view()
+                .frame(width:236, height: 64)
+                .overlay{
+                    //using an SF symbol to use arrow inside the button
+                    Label("Start the course", systemImage: "arrow.forward")
+                        .offset(x: 4, y:4)
+                        .font(.headline)
+                    
+                }
+            
+            //adding a drop shadow to the button
+                .background {
+                    Color.black
+                        .cornerRadius(30)
+                        .blur(radius: 10)
+                        .opacity(0.3)
+                        .offset(y:10)
+                    
+                }
+            
+                //adding the animation to the button
+                .onTapGesture {
+                    button.play(animationName: "active")
+                }
+            
+            
+        }
         
     }
 }
