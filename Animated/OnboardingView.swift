f8c888748363fb25a464ec67e5f0008d98311fa4
fbladimir <franklinbladimirck@yahoo.com>
2022-12-09 18:36:51 -0500

    created custom fonts and used in onboarding

--- Animated/OnboardingView.swift
+++ Animated/OnboardingView.swift
@@ -25,31 +25,55 @@ struct OnboardingView: View {
                         .offset(x:200, y:100)
             )
             
-            //Adding the declared button to view it 
-            button.view()
-                .frame(width:236, height: 64)
-                .overlay{
-                    //using an SF symbol to use arrow inside the button
-                    Label("Start the course", systemImage: "arrow.forward")
-                        .offset(x: 4, y:4)
-                        .font(.headline)
-                    
+            VStack(alignment: .leading, spacing: 16) {
+                
+                Text("Learn Design & Code")
+                    .font(.custom("Poppins Bold", size: 60, relativeTo: .largeTitle))
+                    .frame(width:260, alignment: .leading)
+                
+                Text("Don’t skip design. Learn design and code, by building real apps with React and Swift. Complete courses about the best tools.").customFont(.body)  //Custom font is used from custom font modifier made in customfont file
+                    .opacity(0.7)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                
+                
+                //Adding the declared button to view it
+                button.view()
+                    .frame(width:236, height: 64)
+                    .overlay{
+                        //using an SF symbol to use arrow inside the button
+                        Label("Start the course", systemImage: "arrow.forward")
+                            .offset(x: 4, y:4)
+                            .font(.headline)
+                        
+                    }
+                
+                
+                //adding a drop shadow to the button
+                    .background {
+                        Color.black
+                            .cornerRadius(30)
+                            .blur(radius: 10)
+                            .opacity(0.3)
+                            .offset(y:10)
+                        
+                    }
+                
+                    //adding the animation to the button
+                    .onTapGesture {
+                        button.play(animationName: "active")
                 }
+                
+                Text("Purchase includes access to 30+ courses, 240+ premium tutorials, 120+ hours of videos, source files and certificates.").customFont(.footnote)
+                    .opacity(0.7)
+                
+                
+
+            }
             
-            //adding a drop shadow to the button
-                .background {
-                    Color.black
-                        .cornerRadius(30)
-                        .blur(radius: 10)
-                        .opacity(0.3)
-                        .offset(y:10)
-                    
-                }
-            
-                //adding the animation to the button
-                .onTapGesture {
-                    button.play(animationName: "active")
-                }
+            .padding(40)
+            .padding(.top, 40)
             
             
         }
---
+++ Animated/SignInView.swift
@@ -0,0 +1,20 @@
+//
+//  SignInView.swift
+//  Animated
+//
+//  Created by Franklin Bladimir on 12/9/22.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text("Sign In")
+    }
+}
+
+struct SignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInView()
+    }
+}
