8b372ee13e88a1a0836a5e78e0e3f8f3a498f397
fbladimir <franklinbladimirck@yahoo.com>
2022-12-11 23:05:04 -0500

    added hamburger menu button with working animation

--- Animated/ContentView.swift
+++ Animated/ContentView.swift
@@ -6,10 +6,16 @@
 
 
 import SwiftUI
+import RiveRuntime
 
 struct ContentView: View {
     
     @AppStorage("selectedTab") var selectedTab: Tab = .chat
+    @State var isOpen = false
+    
+    let button = RiveViewModel(fileName: "menu_button", stateMachineName: "State Machine", autoPlay: false)
+    
+    
     var body: some View {
         
         ZStack {
@@ -26,6 +32,17 @@ struct ContentView: View {
             case .user:
                 Text("USER")
             }
+            
+            button.view()
+                .frame(width: 44, height: 44)
+                .mask(Circle())
+                .shadow(color: Color("Shadow").opacity(0.2), radius: 5, x: 0, y: 5)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                .padding() 
+                .onTapGesture {
+                    button.setInput("isOpen", value: isOpen)
+                    isOpen.toggle()
+                }
             
             TabBar()
         }
